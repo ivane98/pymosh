7fae07e45cd097c58366a1b3de7b5df0b120c74c
ivane98 <i.vardoshvili22@gmail.com>
2024-06-25 20:12:46 +0400

    '2606003'

--- pyshop/products/views.py
+++ pyshop/products/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Products
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello World')
+    products = Products.objects.all()
+    return render(request, 'index.html', {'products':products})
 
 def new_products(request):
     return HttpResponse('New Product')
